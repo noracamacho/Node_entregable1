5e001e6efa4721d83f082572fa6bdaa392cda8d9
noracamacho <ingnorac@gmail.com>
2023-05-01 21:54:01 -0600

    first commit

--- blog.sql
+++ blog.sql
@@ -1,5 +1,7 @@
+-- Crear base de datos
 CREATE DATABASE blog_db;
 
+-- Crear Tabla users
 CREATE TABLE users (
 id serial primary key,
 first_name varchar(30) NOT NULL,
@@ -7,6 +9,7 @@ last_name varchar(30) NOT NULL,
 email varchar(50) UNIQUE NOT NULL
 );
 
+-- Crear Tabla posts
 CREATE TABLE posts (
 id serial primary key,
 creator_id int NOT NULL,
@@ -15,6 +18,7 @@ text varchar(200) NOT NULL,
 date date
 );
 
+-- Crear tabla likes
 CREATE TABLE likes (
 id serial primary key,
 user_id int NOT NULL,
@@ -25,12 +29,14 @@ ALTER TABLE posts ADD FOREIGN KEY (creator_id) REFERENCES users (id);
 ALTER TABLE likes ADD FOREIGN KEY (user_id) REFERENCES users (id);
 ALTER TABLE likes ADD FOREIGN KEY (post_id) REFERENCES posts (id);
 
+-- Instertar datos de users
 INSERT INTO users (first_name, last_name, email) 
 VALUES 
 ('Jürgen', 'Smith', 'smith@gmail.com'), 
 ('Lara', 'Croft', 'croft@gmail.com'), 
 ('Alejandro', 'Arias', 'alex@gmail.com');
 
+-- Insertar datos de posts
 INSERT INTO posts (creator_id, title, text, date) 
 VALUES 
 (1, 'Post 1', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. In quis laoreet velit. Pellentesque euismod neque metus, non elementum ex vulputate eu. Vestibulum mattis ac orci vitae rhoncusa.', '2023-02-20'),
@@ -39,15 +45,8 @@ VALUES
 (3, 'Post 4', 'Lorem jh usksdhf  ipsukjhhgjk jhgljhh uygiyrders, consectetur adipiscing elit. In quis laoreet velit. Pellentesque euismod neque metus, non elementum ex vulputate eu. Vestibulum mattis ac orci vitae rhoncus jhgslj dlska.', '2023-04-16'),
 (1, 'Post 5', 'Lorem heie jh usksdhf ipsukjhhgjk jhgljhh uygiyrders, consectetur adipiscing elit. In quis laoreet velit. Pellentesque euisjdshfbv mod neque metus, non elementum ex vulputate eu. Vestibulum mattis a.', '2023-04-21');
 
-INSERT INTO posts (creator_id, title, text, date) 
-VALUES 
-(1, 'Post 1', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. In quis laoreet velit. Pellentesque euismod neque metus.', '2023-02-20'),
-(3, 'Post 2', 'Lorem ipsum neque metus, non elementum ex vulputate eu. Vestibulum mattis ac orci vitae rhoncusa, Pellentesque', '2023-02-20'),
-(2, 'Post 3', 'Lorem ipsukjhhgjk jhgljhh uygiyrders, consectetur adipiscing elit. In quis laoreet velit. Pemattis ac orci vitae rhoncusa.', '2023-04-02'),
-(3, 'Post 4', 'Lorem jh usksdhf  ipsukjhhgjk jhgljhh uygiyrders, consectetur adipiscing elimattis ac orci vitae rhoncus jhgslj dlska.', '2023-04-16'),
-(1, 'Post 5', 'Lorem heie jh usksdhf ipsukjhhgjk jhgljhh uygiyrders, consectetur adipiscing elit. In qui. Vestibulum mattis a.', '2023-04-21');
-
-INSERT INTO posts (user_id, post_id) 
+-- Instertar datos de likes
+INSERT INTO likes (user_id, post_id) 
 VALUES 
 (1,5),
 (2,1),
@@ -55,6 +54,7 @@ VALUES
 (3,4),
 (2,5);
 
+-- Trae todos los posts y la información del usuario del campo creator_id
 SELECT * FROM posts INNER JOIN users on posts.creator_id = users.id;
 
  id | creator_id | title  |                                                                                                   text                                                                                                   |    date    | id | first_name | last_name |      email      
@@ -67,8 +67,9 @@ SELECT * FROM posts INNER JOIN users on posts.creator_id = users.id;
 (5 rows)
 
 
-SELECT * FROM likes INNER JOIN posts ON likes.post_id = posts.id INNER JOIN users ON likes.user_id = users.id;
+-- SELECT * FROM likes INNER JOIN posts ON likes.post_id = posts.id INNER JOIN users ON likes.user_id = users.id;
 
+-- Trae todos los posts, con la información de los usuarios que les dieron like.
 SELECT * FROM likes INNER JOIN posts ON likes.post_id = posts.id INNER JOIN users ON likes.user_id = users.id;
  id | user_id | post_id | id | creator_id | title  |                                                                                                   text                                                                                                   |    date    | id | first_name | last_name |      email      
 ----+---------+---------+----+------------+--------+----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------+------------+----+------------+-----------+-----------------
